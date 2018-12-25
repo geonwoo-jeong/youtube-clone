4e6e7fb0478396deebbfff4e115a8aa67326f5ab
Geonwoo Jeong <geonwoo.jeong@gmail.com>
2018-12-25 20:33:54 +0900

    MVC Pattern part Two

--- Routers/videoRouter.ts
+++ Routers/videoRouter.ts
@@ -1,5 +1,12 @@
 import { Router } from "express";
+import routes from "../routes";
 
 const videoRouter = Router();
+
+videoRouter.get(routes.videos, (req, res) => res.send("Videos"));
+videoRouter.get(routes.upload, (req, res) => res.send("Upload"));
+videoRouter.get(routes.videoDetail, (req, res) => res.send("Video Detail"));
+videoRouter.get(routes.editVideo, (req, res) => res.send("Edit Video"));
+videoRouter.get(routes.deleteVideo, (req, res) => res.send("Delete Video"));
 
 export default videoRouter;
--- app.ts
+++ app.ts
@@ -4,6 +4,7 @@ import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
 import { userRouter, videoRouter, globalRouter } from "./Routers";
+import routes from "./routes";
 
 const app = express();
 
@@ -13,8 +14,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(morgan("dev"));
 
-app.use("/", globalRouter);
-app.use("/user", userRouter);
-app.use("/video", videoRouter);
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
 export default app;
