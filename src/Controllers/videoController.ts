16a08a829a0dd77a10e402e3b8b50c0bef8acf87
Geonwoo Jeong <geonwoo.jeong@gmail.com>
2019-04-24 20:49:36 +0900

    Search Controller

--- src/Controllers/videoController.ts
+++ src/Controllers/videoController.ts
@@ -1,16 +1,26 @@
 import { Request, Response } from "express";
 
 export const home = (req: Request, res: Response) =>
-  res.render("home", { pageTitle: "Home" });
-export const search = (req: Request, res: Response) =>
-  res.render("Search", { pageTitle: "Search" });
+  res.render("Home", { pageTitle: "Home" });
+
+export const search = (req: Request, res: Response) => {
+  const {
+    query: { term }
+  } = req;
+  res.render("Search", { pageTitle: "Search", term });
+};
+
 export const videos = (req: Request, res: Response) =>
   res.render("Videos", { pageTitle: "Vidoes" });
+
 export const upload = (req: Request, res: Response) =>
   res.render("Upload", { pageTitle: "Upload" });
+
 export const videoDetail = (req: Request, res: Response) =>
   res.render("Video Detail", { pageTitle: "Video Detail" });
+
 export const editVideo = (req: Request, res: Response) =>
   res.render("Edit Video", { pageTitle: "Edit Video" });
+
 export const deleteVideo = (req: Request, res: Response) =>
   res.render("Delete Video", { pageTitle: "Delete Video" });
