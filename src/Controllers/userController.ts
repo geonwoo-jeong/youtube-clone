6899c015a19fcfb41ff5b4ad4995b946c22930b2
Geonwoo Jeong <geonwoo.jeong@gmail.com>
2019-04-24 20:34:51 +0900

    Template Variables in Pug

--- src/Controllers/userController.ts
+++ src/Controllers/userController.ts
@@ -1,12 +1,16 @@
 import { Request, Response } from "express";
 
-export const join = (req: Request, res: Response) => res.send("Join");
-export const login = (req: Request, res: Response) => res.send("Login");
-export const logout = (req: Request, res: Response) => res.send("Logout");
-export const users = (req: Request, res: Response) => res.send("Users");
+export const join = (req: Request, res: Response) =>
+  res.render("Join", { pageTitle: "Join" });
+export const login = (req: Request, res: Response) =>
+  res.render("Login", { pageTitle: "Login" });
+export const logout = (req: Request, res: Response) =>
+  res.render("Logout", { pageTitle: "Logout" });
+export const users = (req: Request, res: Response) =>
+  res.render("Users", { pageTitle: "Users" });
 export const userDetail = (req: Request, res: Response) =>
-  res.send("User Detail");
+  res.render("User Detail", { pageTitle: "User Detail" });
 export const editProfile = (req: Request, res: Response) =>
-  res.send("Edit Profile");
+  res.render("Edit Profile", { pageTitle: "Edit Profile" });
 export const changePassword = (req: Request, res: Response) =>
-  res.send("Change Password");
+  res.render("Change Password", { pageTitle: "Change Password" });
--- src/Controllers/videoController.ts
+++ src/Controllers/videoController.ts
@@ -1,12 +1,16 @@
 import { Request, Response } from "express";
 
-export const home = (req: Request, res: Response) => res.render("home");
-export const search = (req: Request, res: Response) => res.send("Search");
-export const videos = (req: Request, res: Response) => res.send("Videos");
-export const upload = (req: Request, res: Response) => res.send("Upload");
+export const home = (req: Request, res: Response) =>
+  res.render("home", { pageTitle: "Home" });
+export const search = (req: Request, res: Response) =>
+  res.render("Search", { pageTitle: "Search" });
+export const videos = (req: Request, res: Response) =>
+  res.render("Videos", { pageTitle: "Vidoes" });
+export const upload = (req: Request, res: Response) =>
+  res.render("Upload", { pageTitle: "Upload" });
 export const videoDetail = (req: Request, res: Response) =>
-  res.send("Video Detail");
+  res.render("Video Detail", { pageTitle: "Video Detail" });
 export const editVideo = (req: Request, res: Response) =>
-  res.send("Edit Video");
+  res.render("Edit Video", { pageTitle: "Edit Video" });
 export const deleteVideo = (req: Request, res: Response) =>
-  res.send("Delete Video");
+  res.render("Delete Video", { pageTitle: "Delete Video" });
