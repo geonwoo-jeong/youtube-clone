4ded282600badc5e737f5bed86929a7b6cbea608
Geonwoo Jeong <geonwoo.jeong@gmail.com>
2019-05-07 23:31:09 +0800

    webpack tes

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,6 +1,10 @@
+// import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import path from "path";
 import webpack from "webpack";
 
+// type Mode = "development" | "production" | "none";
+
+const MODE = process.env.WEBPACK_ENV;
 const ENTRY_FILE = path.resolve(__dirname, "assets", "ts", "main.ts");
 const OUTPUT_DIR = path.join(__dirname, "static");
 
