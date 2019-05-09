9713d302b1bf89c4ef37b1c1649d58497f7a8de9
Geonwoo Jeong <geonwoo.jeong@gmail.com>
2019-05-09 22:41:53 +0800

    Webpack Test 2

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,18 +1,61 @@
-// import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import autoprefixer from "autoprefixer";
+import { CheckerPlugin } from "awesome-typescript-loader";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import path from "path";
 import webpack from "webpack";
 
-// type Mode = "development" | "production" | "none";
+type Mode = "development" | "production" | "none";
 
 const MODE = process.env.WEBPACK_ENV;
 const ENTRY_FILE = path.resolve(__dirname, "assets", "ts", "main.ts");
 const OUTPUT_DIR = path.join(__dirname, "static");
 
 const config: webpack.Configuration = {
+  devtool: "source-map",
   entry: ENTRY_FILE,
+  mode: MODE as Mode,
+  module: {
+    rules: [
+      {
+        exclude: /node_modules/,
+        test: /\.(ts)$/,
+        use: "awesome-typescript-loader"
+      },
+      {
+        test: /\.(scss)$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader"
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              plugin() {
+                return [autoprefixer({ browsers: "cover 99.5%" })];
+              }
+            }
+          },
+          {
+            loader: "scss-loader"
+          }
+        ]
+      }
+    ]
+  },
   output: {
-    filename: "[name].[format]",
+    filename: "[name].js",
     path: OUTPUT_DIR
+  },
+  plugins: [
+    new CheckerPlugin(),
+    new MiniCssExtractPlugin({
+      chunkFilename: "[name].css",
+      filename: "styles.css"
+    })
+  ],
+  resolve: {
+    extensions: ["ts", "js"]
   }
 };
 
