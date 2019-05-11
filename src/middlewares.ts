04b9e4eadcd7d53a95ee9461e7580f30d34d8988
Geonwoo Jeong <geonwoo.jeong@gmail.com>
2019-05-11 19:14:52 +0800

    Footer and Login / Join

--- src/middlewares.ts
+++ src/middlewares.ts
@@ -8,7 +8,7 @@ export const localMiddlewares: express.RequestHandler = (req, res, next) => {
   res.locals.siteName = "YouTube-Clone";
   res.locals.routes = routes;
   res.locals.user = {
-    isAuthenticated: true,
+    isAuthenticated: false,
     id: 1
   };
   next();
