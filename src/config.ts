63ff59c59dd19e4bcb793c320a4b8cf41df376b7
Geonwoo Jeong <geonwoo.jeong@gmail.com>
2019-05-17 20:09:29 +0800

    Fix Typo

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 import dotenv from "dotenv";
 import path from "path";
 
-const NODE_ENV = process.env.NODE_NEV;
+const NODE_ENV = process.env.NODE_ENV;
 
 if (typeof NODE_ENV === "undefined") {
   throw new Error("NODE_ENV is undefined");
