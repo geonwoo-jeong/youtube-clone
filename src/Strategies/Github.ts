5f06f06c1a8c9384aca79a95a378fc329566c838
Geonwoo Jeong <geonwoo.jeong@gmail.com>
2019-05-20 19:21:23 +0800

    Github Log In part Four + 1

--- src/Strategies/Github.ts
+++ src/Strategies/Github.ts
@@ -1,7 +1,55 @@
 import passport from "passport";
 import github from "passport-github2";
-import User from "../Models/User"
+import User from "../Models/User";
 import routes from "../routes";
+
+interface IGithubProfile extends passport.Profile {
+  id: string;
+  displayName: string;
+  username: string;
+  profileUrl: string;
+  email: Array<{
+    value: string;
+  }>;
+  photo: {
+    value: string[];
+  };
+  provider: string;
+  _raw: string;
+  _json: {
+    login: string;
+    id: number;
+    node_id: string;
+    avatar_url: string;
+    gravatar_id: string;
+    url: string;
+    html_url: string;
+    followers_url: string;
+    following_url: string;
+    gists_url: string;
+    starred_url: string;
+    subscription_url: string;
+    organizations_url: string;
+    repo_url: string;
+    events_url: string;
+    received_events_url: string;
+    type: string;
+    site_admin: boolean;
+    name: string;
+    company: string;
+    blog: string;
+    location: string;
+    email: string;
+    hireable?: string;
+    bio?: string;
+    public_repos: number;
+    public_gits: number;
+    followers: number;
+    following: number;
+    created_at: string;
+    updated_at: string;
+  };
+}
 
 const callbackURL = process.env.PASSPORT_GITHUB_CALLBACK_URL;
 const clientID = process.env.PASSPORT_GITHUB_CLIENT;
@@ -33,28 +81,30 @@ const GithubStrategyOptions: github.StrategyOptions = {
 const githubLoginCallBack = async (
   accessToken: string,
   refreshToken: string,
-  profile: github.Profile,
-  done: (error: any, user?: any) => void
+  profile: IGithubProfile,
+  callback: any
 ) => {
   const {
     _json: { id, avatar_url, name, email }
   } = profile;
   try {
-    const user = await User.find({ email})
+    const user = await User.findOne({ email });
     if (user) {
-      user.githubId = id;
-      user.save();
-      return cb(null, user);
+      if (!user.githubId) {
+        user.githubId = id;
+        user.save();
+      }
+      return callback(null, user);
     }
-
     const newUser = await User.create({
+      avatarUrl: avatar_url,
       email,
-      name,
       githubId: id,
-      avatarUrl: avatar_url
-    })
-    return cb(null, newUser);
-    
+      name
+    });
+    return callback(null, newUser);
+  } catch (error) {
+    return callback(error);
   }
 };
 
