74cc6e38e4ecf76d8858702fd10abc216f2f5f30
Geonwoo Jeong <geonwoo.jeong@gmail.com>
2019-05-22 20:20:09 +0800

    User Profile

--- src/middlewares.ts
+++ src/middlewares.ts
@@ -7,7 +7,7 @@ const multerVideo = multer({ dest: "uploads/videos/" });
 export const localMiddlewares: express.RequestHandler = (req, res, next) => {
   res.locals.siteName = "YouTube-Clone";
   res.locals.routes = routes;
-  res.locals.user = req.user || null;
+  res.locals.loggedUser = req.user || null;
   next();
 };
 
