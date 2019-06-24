cb67f3feab452aeab45953f4e1f198368b101347
Geonwoo Jeong <geonwoo.jeong@gmail.com>
2019-06-24 17:19:25 +0900

    Mute Unmute

--- src/Assets/ts/videoPlayer.ts
+++ src/Assets/ts/videoPlayer.ts
@@ -5,20 +5,38 @@ const videoPlayer: HTMLVideoElement | null = document.querySelector(
   "#jsVideoPlayer video"
 );
 const playBtn: HTMLSpanElement | null = document.getElementById("jsPlayButton");
+const volumeBtn: HTMLSpanElement | null = document.getElementById(
+  "jsVolumeButton"
+);
 
 const handlePlayClick = () => {
-  if (videoPlayer) {
+  if (videoPlayer && playBtn) {
     if (videoPlayer.paused) {
       videoPlayer.play();
+      playBtn.innerHTML = '<i class="fas fa-pause"></i>';
     } else {
       videoPlayer.pause();
+      playBtn.innerHTML = '<i class="fas fa-play"></i>';
+    }
+  }
+};
+
+const handleVolumeClick = () => {
+  if (videoPlayer && volumeBtn) {
+    if (videoPlayer.muted) {
+      videoPlayer.muted = false;
+      volumeBtn.innerHTML = '<i class="fas fa-volume-up"></i>';
+    } else {
+      videoPlayer.muted = true;
+      volumeBtn.innerHTML = '<i class="fas fa-volume-mute"></i>';
     }
   }
 };
 
 const init = () => {
-  if (playBtn) {
+  if (playBtn && volumeBtn) {
     playBtn.addEventListener("click", handlePlayClick);
+    volumeBtn.addEventListener("click", handleVolumeClick);
   }
 };
 
--- static/main.js
+++ static/main.js
@@ -125,19 +125,35 @@ __webpack_require__(/*! ./videoPlayer.ts */ "./src/Assets/ts/videoPlayer.ts");
 const videoContainer = document.getElementById("jsVideoPlayer");
 const videoPlayer = document.querySelector("#jsVideoPlayer video");
 const playBtn = document.getElementById("jsPlayButton");
+const volumeBtn = document.getElementById("jsVolumeButton");
 const handlePlayClick = () => {
-    if (videoPlayer) {
+    if (videoPlayer && playBtn) {
         if (videoPlayer.paused) {
             videoPlayer.play();
+            playBtn.innerHTML = '<i class="fas fa-pause"></i>';
         }
         else {
             videoPlayer.pause();
+            playBtn.innerHTML = '<i class="fas fa-play"></i>';
+        }
+    }
+};
+const handleVolumeClick = () => {
+    if (videoPlayer && volumeBtn) {
+        if (videoPlayer.muted) {
+            videoPlayer.muted = false;
+            volumeBtn.innerHTML = '<i class="fas fa-volume-up"></i>';
+        }
+        else {
+            videoPlayer.muted = true;
+            volumeBtn.innerHTML = '<i class="fas fa-volume-mute"></i>';
         }
     }
 };
 const init = () => {
-    if (playBtn) {
+    if (playBtn && volumeBtn) {
         playBtn.addEventListener("click", handlePlayClick);
+        volumeBtn.addEventListener("click", handleVolumeClick);
     }
 };
 if (videoContainer) {
