d13084dc3eaccc20f2772bf090403ca58a8fbd1e
Geonwoo Jeong <geonwoo.jeong@gmail.com>
2019-06-26 20:03:18 +0900

    Recording video

--- src/Assets/ts/videoRecorder.ts
+++ src/Assets/ts/videoRecorder.ts
@@ -6,7 +6,35 @@ const videoPreview = document.getElementById(
   "jsVideoPreview"
 ) as HTMLVideoElement;
 
-const startRecording = async () => {
+let streamObject: MediaStream;
+let videoRecorder: MediaRecorder;
+
+const handleVideoData = (event: any): void => {
+  console.log("handle Video Data");
+  const { data: videoFile } = event;
+  const link = document.createElement("a");
+  link.href = URL.createObjectURL(videoFile);
+  link.download = "recorded.webm";
+  document.body.appendChild(link);
+  link.click();
+};
+
+const stopRecording = (): void => {
+  console.log("stop Recording");
+  videoRecorder.stop();
+  recordBtn.removeEventListener("click", stopRecording);
+  recordBtn.addEventListener("click", getVideo);
+  recordBtn.innerHTML = "Start recording";
+};
+
+const startRecording = (): void => {
+  videoRecorder = new MediaRecorder(streamObject);
+  videoRecorder.start();
+  videoRecorder.addEventListener("dataavailable", handleVideoData);
+  recordBtn.addEventListener("click", stopRecording);
+};
+
+const getVideo = async (): Promise<void> => {
   try {
     const stream = await navigator.mediaDevices.getUserMedia({
       audio: true,
@@ -15,14 +43,18 @@ const startRecording = async () => {
     videoPreview.srcObject = stream;
     videoPreview.muted = true;
     videoPreview.play();
+    recordBtn.innerHTML = "Stop recording";
+    streamObject = stream;
+    startRecording();
   } catch (error) {
-    recordBtn.innerHTML = "Can't Record";
-    recordBtn.removeEventListener("click", startRecording);
+    recordBtn.innerHTML = "☹️ Can't Record";
+  } finally {
+    recordBtn.removeEventListener("click", getVideo);
   }
 };
 
-const init = () => {
-  recordBtn.addEventListener("click", startRecording);
+const init = (): void => {
+  recordBtn.addEventListener("click", getVideo);
 };
 
 if (recorderContainer) {
